8044632dc0c0fecf9f70fc8ae474de0ebd13f2f2
Appu13 <42376781+Appu13@users.noreply.github.com>
2020-03-30 21:20:03 +0530

    Add files via upload
    A java program to show typecasting

---
+++ .github/workflows/typecasting.java
@@ -0,0 +1,22 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// wide typecasting
+		int no=6;
+		float s=no;
+		// display
+		System.out.println("Integer Value "+no);
+		System.out.println("Typecasted float "+s);
+		//narrow typecasting
+		float m=(float)9.00;
+		int y=(int) m;
+		//display
+		System.out.println("Float value "+m);
+		System.out.println("Typecasted integer "+y);
+		
+		
+
+	}
+
+}
