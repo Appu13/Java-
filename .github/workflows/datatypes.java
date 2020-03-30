fd06cb9e17b3d010785be441b5341a41fe851700
Appu13 <42376781+Appu13@users.noreply.github.com>
2020-03-30 20:13:57 +0530

    Add files via upload
    Simple java program to create basic datatypes and display them

---
+++ .github/workflows/datatypes.java
@@ -0,0 +1,23 @@
+
+public class datatypes {
+
+	public static void main(String[] args) {
+ //declartion of different datatypes
+		int a=10;
+		float b=(float) 9.9;
+		char s='s';
+		boolean bs=true;
+		String txt="hi";
+		
+		
+		// printing the variables
+		
+		System.out.println("Interger "+a);
+		System.out.println("Floating value "+b);
+		System.out.println("Charectar "+s);
+		System.out.println("Boolean "+bs);
+		System.out.println("String "+txt);
+		
+		}
+
+}
