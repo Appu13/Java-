8d7caa8709d3b544590911da5da2c98987bb0ef5
Appu13 <42376781+Appu13@users.noreply.github.com>
2020-03-30 21:21:51 +0530

    Add files via upload
    A java program to show different operators in Java.

---
+++ .github/workflows/operators.java
@@ -0,0 +1,86 @@
+
+public class operators {
+
+	public static void main(String[] args) {
+		// Declaration of operands
+		int a=5;// First operand
+		int b=3; // Second operand
+	    int c;//To store result of operation
+	    
+	    //Arithmetic Operations
+	    
+	    c=a+b;//Addition
+	    System.out.println("Sum "+c);
+	    
+	    c=a-b;//Subtraction
+	    System.out.println("Difference "+c);
+	    
+
+	    c=a*b;//Multiplication
+	    System.out.println("Product "+c);
+	    
+	    c=a/b;//Division
+	    System.out.println("Qutioent "+c);
+	    
+	    c=a%b;//modulous 
+	    System.out.println("Remainder "+c);
+	    
+	    c++;//increment
+	    System.out.println("Result "+c);
+	    
+	    c--;//decrement
+	    System.out.println("Result "+c);
+	    //Assignment operations
+	
+	     c+=a;//Addition assignment
+	     System.out.println("Sum "+c);
+	    
+	     c-=a;//Subtraction assignment
+	     System.out.println("Difference "+c);
+	    
+	     c*=a;//Multiplication Assignment
+		 System.out.println("Product "+c);
+		 
+		 c/=a;//Division Assignment
+		 System.out.println("Qutioent "+c);
+		 
+		 c%=a;//modulous 
+	     System.out.println("Remainder "+c);
+	    
+          //Comparison Operators
+	     boolean d;
+
+	     d=a==b;// Equal to operator
+	     System.out.println("Result "+d);
+	     
+	     d=a!=b;// Not equal to operator
+	     System.out.println("Result "+d);
+	     
+	     d=a>b;// Greater than operator
+	     System.out.println("Result "+d);
+	     
+	     d=a>=b;// Greater than or equal to operator
+	     System.out.println("Result "+d);
+	     
+	     d=a<b;// Less than operator
+	     System.out.println("Result "+d);
+	     
+	     d=a<=b;// Less than or equal to operator
+	     System.out.println("Result "+d);
+	     
+	     //Logical operation
+	     boolean x=true;
+	     boolean y=true;
+	     d=x&&y;// And operator
+	     System.out.println("Result "+d);
+	     
+	     d=x||y;//Or operator
+	     System.out.println("Result "+d);
+	     
+	     d=!x;// Equal to operator
+	     System.out.println("Result "+d);
+	     
+	    
+	}
+
+}
