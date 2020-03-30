2521cbb2fe2e7ab10e73d88a226c8bff781be59e
Appu13 <42376781+Appu13@users.noreply.github.com>
2020-03-30 21:46:02 +0530

    Add files via upload
    A java program to show different math operations.

---
+++ .github/workflows/maths.java
@@ -0,0 +1,27 @@
+
+public class maths {
+
+	public static void main(String[] args) {
+		// Declaration of the operands
+		int a=-3;
+		int b=4;
+		
+		// To find maximum of two numbers
+		System.out.println("Maximum number "+Math.max(a, b));
+		
+		// To find minimum of two numbers
+	    System.out.println("Minimum number "+Math.min(a, b));
+		
+	 // To find square root number
+	 		System.out.println("Square root "+Math.sqrt(b));
+	 // To generate a random number
+		System.out.println("Random number "+Math.random());
+	// To find absolute value of a number
+		System.out.println("Absolute value "+Math.abs(a));
+						
+	 		
+				
+
+	}
+
+}
