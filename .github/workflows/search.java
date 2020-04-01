8afc49c6b52a1a1e91c5c5c4c8023e5b5367edcb
Appu13 <42376781+Appu13@users.noreply.github.com>
2020-04-01 21:34:02 +0530

    Update search.java

--- .github/workflows/search.java
+++ .github/workflows/search.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
 
-
-
-
-
 public class search{
 	static void issort()
 	{
-		boolean t=true;
-		
-		int n;
+		boolean t=true;// flag variable
+		int n; //size of array
 		Scanner c=new Scanner(System.in);
 		System.out.println("Enter a maximum of ten elements to be inserted into array");
 		n=c.nextInt();
-		try
+		try // to test if array has a correct size
 		{
 		if(n>10)	
 		{
@@ -27,14 +22,14 @@ public class search{
 		}
 		int a[]=new int[n];
 		
-		
+		//inputing array
 		for (int i=0;i<n;i++) {
 			System.out.println("Enter the "+(i+1)+" Element");
 			a[i]=c.nextInt();						
 		}
 		
 		
-
+                 //testing array
 		for (int i=0;i<n-1;i++) {
 			if(a[i]>a[i+1])
 			{
